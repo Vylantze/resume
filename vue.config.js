716b60a0312e27353f6931f619e5b7c24122fed5
Darryl <darryl@vrcollab.com>
2022-01-07 22:01:54 +0800

    Update name of page.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,13 @@
 // vue.config.js
 module.exports = {
   // options...
+  pages: {
+    index: {
+      // entry for the page
+      entry: 'src/main.js',
+      title: 'Darryl\'s Resume',
+    },
+  },
   publicPath: '/resume/',
   pluginOptions: {
     electronBuilder: {
