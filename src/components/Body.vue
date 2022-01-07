cc92cc084e558ae1733571ec577d230248404cd7
Darryl <darryl@vrcollab.com>
2022-01-07 22:44:25 +0800

    Add subsection to prevent the breakup of sections when printing.

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -3,186 +3,208 @@
     <Section
       title="Education"
     >
-      <SectionedLine>
-        <div slot="left">
-          <b>National University of Singapore</b>
+      <Subsection>
+        <SectionedLine>
+          <div slot="left">
+            <b>National University of Singapore</b>
+          </div>
+          <div slot="right">
+            08/2014 – 05/2018
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          Bachelor of Computing (Honours) in Computer Science
         </div>
-        <div slot="right">
-          08/2014 – 05/2018
-        </div>
-      </SectionedLine>
-      <div class="italics note">
-        Bachelor of Computing (Honours) in Computer Science
-      </div>
+      </Subsection>
 
-      <SectionedLine style="margin-top: 10px">
-        <div slot="left">
-          <b>Saint Andrew's Junior College</b>
+      <Subsection>
+        <SectionedLine style="margin-top: 10px">
+          <div slot="left">
+            <b>Saint Andrew's Junior College</b>
+          </div>
+          <div slot="right">
+            01/2010 – 12/2011
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          GCE 'A' Levels - H2 Computing, H2 Maths, H2 Physics
         </div>
-        <div slot="right">
-          01/2010 – 12/2011
-        </div>
-      </SectionedLine>
-      <div class="italics note">
-        GCE 'A' Levels - H2 Computing, H2 Maths, H2 Physics
-      </div>
+      </Subsection>
     </Section>
 
     <Section
       title="Skills"
     >
-      <table>
-        <colgroup>
-          <col span="1" style="width: 20%;">
-          <col span="1" style="width: 80%;">
-        </colgroup>
+      <Subsection>
+        <table>
+          <colgroup>
+            <col span="1" style="width: 20%;">
+            <col span="1" style="width: 80%;">
+          </colgroup>
 
-        <tbody>
-          <tr>
-            <td><b>Languages</b></td>
-            <td>C++, C#, Golang, Java, Javascript, HTML/CSS</td>
-          </tr>
-          <tr>
-            <td><b>Frameworks</b></td>
-            <td>Vuejs, React, Unity Engine, Unreal Engine</td>
-          </tr>
-          <tr>
-            <td><b>Developer Tools</b></td>
-            <td>Git, Heroku, Render, AWS, MySQL, MongoDB</td>
-          </tr>
-        </tbody>
-      </table>
+          <tbody>
+            <tr>
+              <td><b>Languages</b></td>
+              <td>C++, C#, Golang, Java, Javascript, HTML/CSS</td>
+            </tr>
+            <tr>
+              <td><b>Frameworks</b></td>
+              <td>Vuejs, React, Unity Engine, Unreal Engine</td>
+            </tr>
+            <tr>
+              <td><b>Developer Tools</b></td>
+              <td>Git, Heroku, Render, AWS, MySQL, MongoDB</td>
+            </tr>
+          </tbody>
+        </table>
+      </Subsection>
     </Section>
 
     <Section
       title="Experience"
     >
-      <SectionedLine>
-        <div slot="left">
-          <b>VRcollab Pte. Ltd.</b>
+      <Subsection>
+        <SectionedLine>
+          <div slot="left">
+            <b>VRcollab Pte. Ltd.</b>
+          </div>
+          <div slot="right">
+            08/2018 – Present
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          Senior Developer
         </div>
-        <div slot="right">
-          08/2018 – Present
-        </div>
-      </SectionedLine>
-      <div class="italics note">
-        Senior Developer
-      </div>
-      <ul>
-        <li>Developed and maintained a full-stack application using Vuejs to manage user accounts.</li>
-        <li>Developed and maintained an Electron application using Vuejs and NodeJS to provide a front-end UI for the desktop application.</li>
-        <li>Developed and maintained a server's apis using Golang to manage issue creation and cloud syncing with the desktop application.</li>
-        <li>Developed and maintained plugins using C# to retrieve and convert geometry data from external applications such as Autodesk Revit.</li>
-      </ul>
+        <ul>
+          <li>Developed and maintained a full-stack application using Vuejs to manage user accounts.</li>
+          <li>Developed and maintained an Electron application using Vuejs and NodeJS to provide a front-end UI for the desktop application.</li>
+          <li>Developed and maintained a server's apis using Golang to manage issue creation and cloud syncing with the desktop application.</li>
+          <li>Developed and maintained plugins using C# to retrieve and convert geometry data from external applications such as Autodesk Revit.</li>
+        </ul>
+      </Subsection>
 
-      <SectionedLine>
-        <div slot="left">
-          <b>Landshark Games</b>
+      <Subsection>
+        <SectionedLine>
+          <div slot="left">
+            <b>Landshark Games</b>
+          </div>
+          <div slot="right">
+            05/2017 – 10/2017
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          Intern
         </div>
-        <div slot="right">
-          05/2017 – 10/2017
-        </div>
-      </SectionedLine>
-      <div class="italics note">
-        Intern
-      </div>
-      <ul>
-        <li>Recreated one of the company's Facebook flash games, Bad Candy, in Unity, using C#.</li>
-        <li>Developed the entire system that supports the game from scratch, which includes the mechanics of the puzzles, menu handling, user input, etc.</li>
-      </ul>
+        <ul>
+          <li>Recreated one of the company's Facebook flash games, Bad Candy, in Unity, using C#.</li>
+          <li>Developed the entire system that supports the game from scratch, which includes the mechanics of the puzzles, menu handling, user input, etc.</li>
+        </ul>
+      </Subsection>
     </Section>
 
     <Section
       title="Projects"
     >
-      <SectionedLine>
-        <div slot="left">
-          <b>KC Simulator</b>
+      <Subsection>
+        <SectionedLine>
+          <div slot="left">
+            <b>KC Simulator</b>
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          <a href="https://github.com/Vylantze/KCS">https://github.com/Vylantze/KCS</a>
         </div>
-      </SectionedLine>
-      <div class="italics note">
-        <a href="https://github.com/Vylantze/KCS">https://github.com/Vylantze/KCS</a>
-      </div>
-      <ul>
-        <li>A rebuild of the home room of the game Kantai Collection.</li>
-        <li>Solo project.</li>
-        <li>Developed using Vuejs to manage components and HTML5 Canvas to replicate animations.</li>
-        <li>Modularized character handling to allow for easy expansion and adding of new characters.</li>
-      </ul>
+        <ul>
+          <li>A rebuild of the home room of the game Kantai Collection.</li>
+          <li>Solo project.</li>
+          <li>Developed using Vuejs to manage components and HTML5 Canvas to replicate animations.</li>
+          <li>Modularized character handling to allow for easy expansion and adding of new characters.</li>
+        </ul>
+      </Subsection>
 
-      <SectionedLine>
-        <div slot="left">
-          <b>OraCle</b>
+      <Subsection>
+        <SectionedLine>
+          <div slot="left">
+            <b>OraCle</b>
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          <a href="https://github.com/CS2103-Aug2015-w15-4j/main">https://github.com/CS2103-Aug2015-w15-4j/main</a>
         </div>
-      </SectionedLine>
-      <div class="italics note">
-        <a href="https://github.com/CS2103-Aug2015-w15-4j/main">https://github.com/CS2103-Aug2015-w15-4j/main</a>
-      </div>
-      <ul>
-        <li>An application that allows for user to input and manage tasks.</li>
-        <li>Made using a team of four.</li>
-        <li>Developed using Java.</li>
-        <li>Developer for user input and dynmically generated UI.</li>
-      </ul>
+        <ul>
+          <li>An application that allows for user to input and manage tasks.</li>
+          <li>Made using a team of four.</li>
+          <li>Developed using Java.</li>
+          <li>Developer for user input and dynmically generated UI.</li>
+        </ul>
+      </Subsection>
 
-      <SectionedLine>
-        <div slot="left">
-          <b>8th Sea</b>
+      <Subsection>
+        <SectionedLine>
+          <div slot="left">
+            <b>8th Sea</b>
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          <a href="https://github.com/Voxsam/8thsea">https://github.com/Voxsam/8thsea</a>
         </div>
-      </SectionedLine>
-      <div class="italics note">
-        <a href="https://github.com/Voxsam/8thsea">https://github.com/Voxsam/8thsea</a>
-      </div>
-      <ul>
-        <li>A game that uses an underwater submarine to catch fish.</li>
-        <li>Made using a team of five.</li>
-        <li>Developed using Unity Engine.</li>
-        <li>Developer for mechanics, as well as merging handler and main debugger.</li>
-      </ul>
+        <ul>
+          <li>A game that uses an underwater submarine to catch fish.</li>
+          <li>Made using a team of five.</li>
+          <li>Developed using Unity Engine.</li>
+          <li>Developer for mechanics, as well as merging handler and main debugger.</li>
+        </ul>
+      </Subsection>
 
-      <SectionedLine>
-        <div slot="left">
-          <b>Voxsam</b>
+      <Subsection>
+        <SectionedLine>
+          <div slot="left">
+            <b>Voxsam</b>
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          <a href="https://github.com/JeanKoh/Voxsam">https://github.com/JeanKoh/Voxsam</a>
         </div>
-      </SectionedLine>
-      <div class="italics note">
-        <a href="https://github.com/JeanKoh/Voxsam">https://github.com/JeanKoh/Voxsam</a>
-      </div>
-      <ul>
-        <li>A game that lets you build towers to defend against viral blobs.</li>
-        <li>Made using a team of six.</li>
-        <li>Developed using Unreal Engine.</li>
-        <li>Developer for mechanics.</li>
-      </ul>
+        <ul>
+          <li>A game that lets you build towers to defend against viral blobs.</li>
+          <li>Made using a team of six.</li>
+          <li>Developed using Unreal Engine.</li>
+          <li>Developer for mechanics.</li>
+        </ul>
+      </Subsection>
 
-      <SectionedLine>
-        <div slot="left">
-          <b>Chaos Children</b>
+      <Subsection>
+        <SectionedLine>
+          <div slot="left">
+            <b>Chaos Children</b>
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          <a href="https://github.com/Vylantze/Chaos-Children">https://github.com/Vylantze/Chaos-Children</a>
         </div>
-      </SectionedLine>
-      <div class="italics note">
-        <a href="https://github.com/Vylantze/Chaos-Children">https://github.com/Vylantze/Chaos-Children</a>
-      </div>
-      <ul>
-        <li>A 2D side-scrolling game that involves fighting enemies and overcoming obstacles.</li>
-        <li>Made using a team of two.</li>
-        <li>Developed using Unity Engine.</li>
-        <li>Developer for user input, character movement and enemy handling.</li>
-      </ul>
+        <ul>
+          <li>A 2D side-scrolling game that involves fighting enemies and overcoming obstacles.</li>
+          <li>Made using a team of two.</li>
+          <li>Developed using Unity Engine.</li>
+          <li>Developer for user input, character movement and enemy handling.</li>
+        </ul>
+      </Subsection>
 
-      <SectionedLine>
-        <div slot="left">
-          <b>Resume</b>
+      <Subsection>
+        <SectionedLine>
+          <div slot="left">
+            <b>Resume</b>
+          </div>
+        </SectionedLine>
+        <div class="italics note">
+          <a href="https://github.com/Vylantze/resume">https://github.com/Vylantze/resume</a>
         </div>
-      </SectionedLine>
-      <div class="italics note">
-        <a href="https://github.com/Vylantze/resume">https://github.com/Vylantze/resume</a>
-      </div>
-      <ul>
-        <li>An online version of this resume.</li>
-        <li>Developed using Vuejs and GitHub pages.</li>
-        <li>Automated building through CI has been implemented.</li>
-      </ul>
+        <ul>
+          <li>An online version of this resume.</li>
+          <li>Developed using Vuejs and GitHub pages.</li>
+          <li>Automated building through CI has been implemented.</li>
+        </ul>
+      </Subsection>
 
     </Section>
   </div>
@@ -191,12 +213,14 @@
 <script>
 import Section from './Section.vue'
 import SectionedLine from './SectionedLine.vue'
+import Subsection from './Subsection.vue'
 
 export default {
   name: 'Body',
   components: {
     Section,
     SectionedLine,
+    Subsection,
   }
 }
 </script>
---
+++ src/components/Subsection.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="subsection">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Subsection'
+}
+</script>
+
+<style scoped>
+.subsection {
+  page-break-inside: avoid;
+}
+</style>
