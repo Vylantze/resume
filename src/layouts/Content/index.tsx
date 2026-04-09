098b0e567eea1cc9b7e129a235d042d95ea3e944
Vylantze <vylantze@users.noreply.github.com>
2026-04-10 04:22:27 +0800

    Add new fields in Experience section.

--- src/layouts/Content/index.tsx
+++ src/layouts/Content/index.tsx
@@ -50,13 +50,59 @@ export function Content () {
         </Section>
 
         <Section title="Experience">
-          <div className={styles['section-content']}>
-            <div className={styles['section-content-left']}>
-              <div className={styles['sub-header']}>UBS AG</div>
-              <div className={styles['note']}>Associate Director</div>
+          <div className={styles['section-experience']}>
+            <div className={styles['section-content']}>
+              <div className={styles['section-content-left']}>
+                <div className={styles['sub-header']}>UBS AG</div>
+                <div className={styles['note']}>Associate Director</div>
+              </div>
+              <div className={styles['section-content-right']}>
+                <span className={styles['duration']}>02/2025 – Present</span>
+              </div>
             </div>
-            <div className={styles['section-content-right']}>
-              <span className={styles['duration']}>02/2025 – Present</span>
+            <div className={styles['section-content-description']}>
+              <ul className="list-disc pl-4">
+                <li>Maintaining an automated transaction system that fully processes transactions without human input, allowing man-hours to only be spent on new cases or errors.</li>
+                <li>Implemented an internal full stack system for access to information about transaction flows, allowing users to view the state of any transaction at any time without the need for developers aid.</li>
+                <li>Introduced and implemented a timeline for the transactions similar to parcel tracking, allowing external parties or new users to easily understand the process transactions go through in the system.</li>
+                <li>Created generic reusable templates for the UI and its backend that allows for easy integration of new transaction flows, reducing the time taken to 1 day.</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className={styles['section-experience']}>
+            <div className={styles['section-content']}>
+              <div className={styles['section-content-left']}>
+                <div className={styles['sub-header']}>Credit Suisse AG</div>
+                <div className={styles['note']}>Software Developer & Engine Staff</div>
+              </div>
+              <div className={styles['section-content-right']}>
+                <span className={styles['duration']}>04/2022 – 01/2025</span>
+              </div>
+            </div>
+            <div className={styles['section-content-description']}>
+              <ul className="list-disc pl-4">
+                <li>Introduced backend optimizations to legacy UI, significantly increasing the initial loading speeds of the pages while minimizing user flow disruption.</li>
+                <li>Refactored legacy UI's implementation with no visible difference to users, allowing for easier maintainability and minimizing missed cases when fixing bugs.</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className={styles['section-experience']}>
+            <div className={styles['section-content']}>
+              <div className={styles['section-content-left']}>
+                <div className={styles['sub-header']}>INFR AI (VRcollab)</div>
+                <div className={styles['note']}>Senior Developer</div>
+              </div>
+              <div className={styles['section-content-right']}>
+                <span className={styles['duration']}>08/2018 – 03/2022</span>
+              </div>
+            </div>
+            <div className={styles['section-content-description']}>
+              <ul className="list-disc pl-4">
+                <li>Co-created a full electron app for project management including token-based authentication and conversions to a proprietary file format, providing increased security for users and from competitors.</li>
+                <li>Exposed to and took up many technologies, encouraging experimentation and cultivating good problem solving habits like breaking down large tasks into smaller sub tasks.</li>
+              </ul>
             </div>
           </div>
         </Section>
