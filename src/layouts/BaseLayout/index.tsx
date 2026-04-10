2971136494da3a643532025d50e1e9665155e91e
Vylantze <vylantze@users.noreply.github.com>
2026-04-11 04:08:44 +0800

    Add an additional bottom text to the printed version.

--- src/layouts/BaseLayout/index.tsx
+++ src/layouts/BaseLayout/index.tsx
@@ -5,6 +5,9 @@ export function BaseLayout ({ children }: PropsWithChildren) {
   return (
     <div className={styles['base-layout']}>
       {children}
+      <div className={styles['print-only-footer']}>
+        This is an offline copy of the resume located at <span className="underline ml-1">https://vylantze.github.io/resume</span>.
+      </div>
     </div>
   );
 };
